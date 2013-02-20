0f02a92f011ae283abc14402f2ad42b45c030054
reassembler <pointblnk@hotmail.com>
2013-02-20 23:32:12 +0000

    MinGW config file, compatible with DirectX

--- cmake/mingw.cmake
+++ cmake/mingw.cmake
@@ -5,6 +5,10 @@ set(lib_base c:/coding/lib)
 set(sdl_root ${lib_base}/SDL-1.2.15)
 set(SDL_INCLUDE_DIR ${lib_base}/SDL-1.2.15/include)
 
+if (NOT DEFINED ENV{DXSDK})
+    message(FATAL_ERROR "Warning: DirectX SDK Variable DXSDK Not Defined!")
+endif()
+
 find_package(SDL REQUIRED)
 
 include_directories("${SDL_INCLUDE_DIR}")
@@ -12,9 +16,11 @@ include_directories("${SDL_INCLUDE_DIR}")
 link_libraries(cannonball 
     ${SDL_LIBRARY}
     SDLmain
+    dxguid
+    dinput8
 )
 
-set(CMAKE_CXX_FLAGS "-Ofast")
+set(CMAKE_CXX_FLAGS "-Ofast -static-libgcc -static-libstdc++")
  
 # Location for Cannonball to create save files
 # Used to auto-generate setup.hpp with various file paths

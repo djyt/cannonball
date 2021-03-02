44d6e404930b28857103bc1804c0e29d28bd2221
DJ YT <blah@blah.com>
2021-03-02 11:24:46 +0000

    Call Play Queued Sound Twice

--- src/main/engine/audio/osoundint.cpp
+++ src/main/engine/audio/osoundint.cpp
@@ -77,6 +77,7 @@ void OSoundInt::tick()
     {
         play_queued_sound(); // Process audio commands from main program code
         osound.tick();
+        play_queued_sound();
         osound.tick();
     }
 }

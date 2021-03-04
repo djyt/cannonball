25ab6a089057cec662e4377243aea06ba7bd3d16
DJ YT <blah@blah.com>
2021-03-04 12:24:22 +0000

    Added some tutorial information to the pi build file

--- cmake/pi4-opengles.cmake
+++ cmake/pi4-opengles.cmake
@@ -1,5 +1,17 @@
 # -----------------------------------------------------------------------------
 # CannonBall Raspberry Pi4 (Smarty Hardware)
+#
+# I am using Visual Studio 2019 and cross-compiling on a PC with VisualGDB 
+# (https://visualgdb.com/) 
+#
+# This works well in terms of being able to use the Visual Studio debugger,
+# but if your environment is different, you may need a different cmake file.
+#
+# I referred to the following tutorial:
+# https://gnutoolchains.com/raspberry/tutorial/
+#
+# I downloaded the 2019-07-10 raspbian-buster-full executable from here:
+# https://gnutoolchains.com/raspberry/
 # -----------------------------------------------------------------------------
 
 # Library Locations

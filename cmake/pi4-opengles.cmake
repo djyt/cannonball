b3854139aa73a125ce96a2fc8ae004333639f593
DJ YT <blah@blah.com>
2022-01-30 19:32:42 +0000

    Revert "Merge pull request #118 from jpouderoux/ImprovedCMake"
    This reverts commit e8ba9b878dc31397cfecca38a386d397af8b7aa4, reversing
    changes made to d329d0b29d4dca8c6b430b0fb3b09a1e3f35fb99.

--- cmake/pi4-opengles.cmake
+++ cmake/pi4-opengles.cmake
@@ -1,8 +1,8 @@
 # -----------------------------------------------------------------------------
 # CannonBall Raspberry Pi4 (Smarty Hardware)
 #
-# I am using Visual Studio 2019 and cross-compiling on a PC with VisualGDB
-# (https://visualgdb.com/)
+# I am using Visual Studio 2019 and cross-compiling on a PC with VisualGDB 
+# (https://visualgdb.com/) 
 #
 # This works well in terms of being able to use the Visual Studio debugger,
 # but if your environment is different, you may need a different cmake file.
@@ -14,16 +14,24 @@
 # https://gnutoolchains.com/raspberry/
 # -----------------------------------------------------------------------------
 
+# Library Locations
+set(lib_base c:/coding/lib)
+set(boost_dir ${lib_base}/boost_1_74_0)
+set(sdl2_dir ${lib_base}/SDL2-2.0.12)
+
 # Use OpenGLES for rendering.
-set(RENDER_ENGINE OpenGLES)
+set(OPENGLES 1)
 
 # GCC Specific flags (optimize for Pi4 CPU)
-#set(CMAKE_CXX_FLAGS "-O3 -mtune=cortex-a72")
+set(CMAKE_CXX_FLAGS "-O3 -mtune=cortex-a72")
 
 # Platform Specific Libraries
 set(platform_link_libs
+    SDL2
+    GLESv2
 )
 
 # Platform Specific Link Directories
 set(platform_link_dirs
+
 )
--- cmake/win64-opengl.cmake
+++ cmake/win64-opengl.cmake
@@ -1,3 +1,25 @@
-# # -----------------------------------------------------------------------------
-# # CannonBall Windows Setup
-# # -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# CannonBall Windows Setup
+# -----------------------------------------------------------------------------
+
+# Library Locations
+set(lib_base c:/coding/lib)
+set(boost_dir ${lib_base}/boost_1_74_0)
+set(sdl2_dir ${lib_base}/SDL2-2.0.12)
+set(dx8_dir c:/dxsdk)
+
+# Use OpenGL for rendering.
+set(OPENGL 1)
+
+# Platform Specific Libraries
+set(platform_link_libs
+    opengl32 # For OpenGL
+    glu32    # For OpenGL
+    dxguid   # Direct X Haptic Support
+    dinput8  # Direct X Haptic Support
+)
+
+# Platform Specific Link Directories
+set(platform_link_dirs
+    "${dx8_dir}/lib"
+)
